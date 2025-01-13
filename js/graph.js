33f2a12545c2bc596de9df2264eee4524c9a6b1a
HyeWon0613 <je38855@naver.com>
2025-01-14 00:36:47 +0900

    [docs]20250113

---
+++ js/graph.js
@@ -0,0 +1,21 @@
+$(".circle-graph").each(function (index, node) {
+  let perNum = $(this).attr("circleProgress");
+  let fillColors = ["#ffb935", "#5e51f3"]; // 그래프의 색상 배열
+  let fillColor = fillColors[index];
+
+  $(this).circleProgress({
+    size: 150, // 그래프 크기
+    value: perNum / 80, // 그래프에 표시될 값
+    startAngle: 300, // 시작지점
+    thickness: 30, // 그래프 두께
+    fill: {
+      // 그래프 선 색
+      color: fillColor,
+    },
+    animation: {
+      duration: 1200,
+    },
+    lineCap: "round", // 그래프 선 모양
+    reverse: false, // 그래프가 진행되는 방향
+  });
+});
