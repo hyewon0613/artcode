dfb874585dcadac87aa2e2cccb605be6d4c4d1f1
hyewon0613 <je38855@naver.com>
2025-02-17 18:21:49 +0900

    [DOCS]두번째 서브페이지 작업중

--- js/concert.js
+++ js/concert.js
@@ -19,8 +19,49 @@ const swiper = new Swiper(".swiper_mySwiper", {
     el: ".swiper-scrollbar",
   },
 
-  autoplay: {
-    delay: 1000,
-    duration: 2500,
+  // autoplay: {
+  //   delay: 1000,
+  //   duration: 3500,
+  // },
+
+  //slidepreview
+  breakpoints: {
+    1440: {
+      slidesPerView: 4,
+      spaceBetween: 60,
+    },
+
+    1025: {
+      slidesPerView: 3,
+      spaceBetween: 10,
+    },
+
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 50,
+    },
+
+    769: {
+      slidesPerView: 3,
+      spaceBetween: 10,
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 10,
+    },
+
+    481: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+
+    480: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    320: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
   },
 });
